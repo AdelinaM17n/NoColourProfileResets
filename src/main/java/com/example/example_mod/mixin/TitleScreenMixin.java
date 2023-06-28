f1df9f64c59538784a3675b419fc6012f9b60597
Maheesha <maheeshasithum@gmail.com>
2023-06-28 19:15:03 +0530

    a crime against java gods that only works once in a blue moon

--- src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/example/example_mod/mixin/TitleScreenMixin.java
@@ -1,19 +1,39 @@
 package com.example.example_mod.mixin;
 
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.example.example_mod.ExampleMod;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
-import net.minecraft.client.gui.screen.TitleScreen;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		try {
+			Field shutdownHook = Display.class.getDeclaredField("shutdown_hook");
+			shutdownHook.setAccessible(true);
+
+			Field modifiersField = Field.class.getDeclaredField("modifiers");
+			modifiersField.setAccessible(true);
+			modifiersField.setInt(shutdownHook, shutdownHook.getModifiers() & ~Modifier.FINAL);
+			modifiersField.setInt(shutdownHook, shutdownHook.getModifiers() & ~Modifier.PRIVATE);
+			shutdownHook.set(Display.class, new Thread(() -> System.out.println("hi")));
+
+			Method registerShutdownHook = Display.class.getDeclaredMethod("registerShutdownHook");
+			registerShutdownHook.setAccessible(true);
+			registerShutdownHook.invoke(null);
+		} catch (NoSuchFieldException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("e");
 	}
 }
